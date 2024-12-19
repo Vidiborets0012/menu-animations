dec8f304625bbee1e2ba34c91ec171ad995a4ae8
Vidiborets Viktor <vidiboretsv@gmail.com>
2024-12-19 09:35:49 +0200

    add styles for the menu

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 const menuButtonOne = document.querySelector('.menu-btn-one');
+const menuOne = document.querySelector('.menu-one');
 const bars = document.querySelectorAll('bar');
 
 menuButtonOne.addEventListener('click', () => {
     menuButtonOne.classList.toggle('active');
+    menuOne.classList.toggle('active');
 
     bars.forEach(bar => {
         bar.classList.toggle('active');
